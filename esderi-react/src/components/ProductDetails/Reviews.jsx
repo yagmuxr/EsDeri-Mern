642d274155be77033152c4795bb8d08f40ac3f2f
yagmur <96817950+yagmuxr@users.noreply.github.com>
2025-02-25 15:51:21 +0300

    tabs-componenti-retouch
    headerdaki fazlalık çıkartıldı, info kısmındaki typo ve fazlalık giderildi

--- esderi-react/src/components/ProductDetails/Reviews.jsx
+++ esderi-react/src/components/ProductDetails/Reviews.jsx
@@ -1,8 +1,8 @@
 import "./ProductDetails.css";
-
-const Reviews = () => {
+import PropTypes from "prop-types";
+const Reviews = ({active}) => {
   return (
-    <div className="tab-panel-reviews content" id="reviews">
+    <div className={`tab-panel-reviews ${active}`}>
       <h3>
         2 reviews for Basic Colored Sweatpants With Elastic Hems
       </h3>
@@ -165,3 +165,6 @@ const Reviews = () => {
 };
 
 export default Reviews;
+Reviews.propTypes = {
+  active: PropTypes.string
+}
